f5bcd6ed9b9b6aa87e5ceda7ea0637d0946c081d
Tung Dao <me@tungdao.com>
2013-10-28 12:28:03 +0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure-logic "0.1.0-SNAPSHOT"
+  :description "Clojure Solutions for Logic Puzzle - http://brownbuffalo.sourceforge.net/puzzle_index.html"
+  :url "http://github.com/tungd/clojure-logic"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.4"]])
